d58bb2cf50870a910abacdaa8d787511102e884f
OverseaMSH <sadegh1384ordibehesht@gmail.com>
2025-05-28 08:08:43 +0330

    Writing the grammer rules and testing Compile.py. It's working for correct grammer but error handling for wrong inputs not working well.

---
+++ Compile.py
@@ -0,0 +1,35 @@
+import re
+GRAMMER_REGEX = [
+    ('EQEQ', r'=='),
+    ('EQ', r'='),
+    ('PLUS', r'\+'),
+    ('MINUS', r'-'),
+    ('STAR', r'\*'),
+    ('DIV', r'/'),
+    ('POW', r'\^'),
+    ('LT', r'<'),
+    ('GT', r'>'),
+    ('LEFT_PAR', r'\('),
+    ('RIGHT_PAR', r'\)'),
+    ('COMMA', r','),
+    ('LITERAL', r'\d+(\.\d+)?'),                  # float or decimal
+    ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*'),    # varibale
+    ('SKIP', r'[ \t\n]+'),                        # space or tabs or new lines
+    ('MISMATCH', r'.'),                           # others
+]
+
+
+def compile(g):
+    grammers = []
+    grammersRegex = '|'.join(
+        f'(?P<{name}>{pattern})' for name, pattern in GRAMMER_REGEX)
+    for i in re.finditer(grammersRegex, g):
+        model = i.lastgroup
+        value = i.group()
+        if model == 'SKIP':
+            continue
+        elif model == 'MISMATCH':
+            raise RuntimeError(f'Unexpected character: {value}')
+        grammers.append((model, value))
+    grammers.append(('EOF', '$'))
+    return grammers
---
+++ main.py
@@ -0,0 +1,8 @@
+from Compile import compile
+code = """x = sin(2.5) + y^2 - 3 /cccc z\
+    
+    
+"""
+tokens = compile(code)
+for tok in tokens:
+    print(tok)
